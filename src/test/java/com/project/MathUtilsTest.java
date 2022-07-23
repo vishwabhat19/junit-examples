ad8d8c529384845529e6182179e7d26eca8afcd8
Vishwa <VB086617>
2022-07-23 22:59:40 +0530

    Changes

--- src/test/java/com/project/MathUtilsTest.java
+++ src/test/java/com/project/MathUtilsTest.java
@@ -1,16 +1,25 @@
 package com.project;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest
 {
+	MathUtils mathUtils;
+
+	@BeforeAll
+	void init() {
+		mathUtils = new MathUtils();
+	}
 
 	@Test
 	void testAdd()
 	{
-		MathUtils mathUtils = new MathUtils();
 		int expected = 2;
 		int actual = mathUtils.add(1,1);
 		assertEquals(expected, actual, "This method should add two numbers!");
@@ -18,13 +27,11 @@ class MathUtilsTest
 
 	@Test
 	void testComputeCircleArea() {
-		MathUtils mathUtils = new MathUtils();
 		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area!");
 	}
 
 	@Test
 	void testDivide() {
-		MathUtils mathUtils = new MathUtils();
 		assertThrows(ArithmeticException.class, () -> {mathUtils.divide(10, 0);});
 	}
 
